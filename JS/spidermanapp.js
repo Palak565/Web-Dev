df84f8307d760d43a2b9943e3398c5532595f7d7
Palak565 <palakgoyal565@gmail.com>
2025-06-01 12:43:59 +0530

    JS Practice

---
+++ JS/spidermanapp.js
@@ -0,0 +1,39 @@
+let mainImg = document.querySelector("#mainImg");
+
+
+mainImg.addEventListener('scroll',function(){
+    console.log('Scroll');
+})
+
+
+let inp = document.querySelector("#inp");
+
+inp.addEventListener('keypress',function(e){
+    console.log(e.key);
+})
+
+
+mainImg.addEventListener('load',function(e){
+    console.log("loaded");
+})
+
+document.addEventListener('scroll',()=>{
+    console.log("scrolled");
+})
+
+let newbtn = document.createElement('button');
+newbtn.innerText = "Click me!";
+
+newbtn.addEventListener('click',()=>{
+    newbtn.style.backgroundColor = 'green';
+})
+
+let body = document.querySelector("body");
+body.append(newbtn);
+
+inp.addEventListener('input', function(){
+    let name = inp.value.replace(/[^a-zA-Z ]/g, '');
+    inp.value = name;
+    let heading = document.querySelector("#heading");
+    heading.innerText = name;
+})
